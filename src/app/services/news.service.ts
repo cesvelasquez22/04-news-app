c67f3cd8abb98dbec305127c66b7bcbeca9159c6
Cesar Velasquez <cesar.velasquez@cit.hn>
2022-01-02 21:48:35 -0600

    + Control interfaces

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { NewsResponse } from './news.interfaces';
+import { NewsResponse } from '@interfaces';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -10,8 +11,11 @@ export class NewsService {
   constructor(private http: HttpClient) {}
 
   getTopHeadlines() {
-    return this.http.get<NewsResponse>(
-      `${environment.apiUrl}/top-headlines?country=us&category=business`, { params: { apiKey: environment.apiKey} }
-    );
+    return this.http
+      .get<NewsResponse>(
+        `${environment.apiUrl}/top-headlines?country=us&category=business`,
+        { params: { apiKey: environment.apiKey } }
+      )
+      .pipe(map(({ articles }) => articles));
   }
 }
