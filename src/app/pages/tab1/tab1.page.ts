dae1a0361915e2b122c18e7f5317dc350bb5701b
Cesar Velasquez <cesar.velasquez@cit.hn>
2022-01-02 22:13:28 -0600

    + Show articles

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '@interfaces';
 import { NewsService } from '@services';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page implements OnInit {
+  articles: Article[] = [];
 
   constructor(private newsService: NewsService) {}
 
   ngOnInit(): void {
-    this.newsService.getTopHeadlines().subscribe(console.log);
+    this.newsService
+      .getTopHeadlines()
+      .subscribe((articles) => this.articles.push(...articles));
   }
-
 }
