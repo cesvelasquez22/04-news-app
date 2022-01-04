f6aa77660c9ad8acd6290050b635b76ec4c48df5
Cesar Velasquez <cesar.velasquez@cit.hn>
2022-01-04 11:01:16 -0600

    + Remove duplicates in storage

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -21,7 +21,18 @@ export class StorageService {
   }
 
   async saveOrRemoveArticle(article: Article) {
-    this._localArticles = [article, ...this._localArticles];
+    const exists = this._localArticles.find(
+      (localArticle) => localArticle.title === article.title
+    );
+
+    if (exists) {
+      this._localArticles = this._localArticles.filter(
+        (localArticle) => localArticle.title !== article.title
+      );
+    } else {
+      this._localArticles = [article, ...this._localArticles];
+    }
+
     this._storage.set('ARTICLES', this._localArticles);
   }
 }
